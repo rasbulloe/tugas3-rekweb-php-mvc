65e7601091d32e173a064fc0d506b831703966b4
Dede Aminudin <rasbulloe@gmail.com>
2019-10-03 17:21:45 +0700

    controller

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php 
+
+class About {
+
+	public function index($nama, $pekerjaan) {
+		echo "Halo , nama saya $nama , saya adalah $pekerjaan";
+	}
+
+	public function page() {
+		echo 'About/page';
+	}
+}
+
+ ?>
